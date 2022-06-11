a27da592a781fabf1c2d5b092bd19f852bdea459
Luciana Santos <luciydss@hotmail.com>
2022-06-11 01:09:13 -0300

    atualização no template

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,12 +7,12 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('index.html'),
-      this.destinationPath('index.html'),
+      this.templatePath('src/index.html'),
+      this.destinationPath('src/index.html'),
     );
     this.fs.copy(
-      this.templatePath('gulpfile.js'),
-      this.destinationPath('gulpfile.js'),
+      this.templatePath('.eslintrc.json'),
+      this.destinationPath('.eslintrc.json'),
     );
     this.fs.copy(
       this.templatePath('_gitignore'),
@@ -23,24 +23,21 @@ module.exports = class extends Generator {
       this.destinationPath('package.json'),
     );
     this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js'),
+    );
+    this.fs.copy(
       this.templatePath('package-lock.json'),
       this.destinationPath('package-lock.json'),
     );
-    this.fs.copy(this.templatePath('app'), this.destinationPath('app'));
+    this.fs.copy(this.templatePath('src/app'), this.destinationPath('src/app'));
   }
 
   end() {
     this.log(' ');
-    this.log(
-      'Finished generating! Before running, make sure you have the Gulp CLI installed globally, and you do NOT have Gulp itself installed globally.',
-    );
+    this.log('Tudo certo!');
     this.log(' ');
-    this.log("If you're not sure how to do this, read this for more info:");
-    this.log('https://coder-coder.com/install-gulp-globally-on-windows/');
-    this.log(' ');
-    this.log(
-      "Once you have the Gulp CLI installed globally, you can run 'gulp' in this project directory to run Gulp!",
-    );
+    this.log('Só executar o npm start e ser feliz!');
     this.log(' ');
   }
 };
